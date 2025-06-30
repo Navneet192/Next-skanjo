9c1c346613b14cb63b2c72cd29598f83598214f7
navneet yadav <105013664+navnt00@users.noreply.github.com>
2025-06-30 11:52:45 +0530

    managed error

--- skonjaassignment/src/pages/Index.tsx
+++ skonjaassignment/src/pages/Index.tsx
@@ -1,9 +1,8 @@
-
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import FeaturesSection from '@/components/FeaturesSection';
 import StatsSection from '@/components/StatsSection';
-import CTASection from '@/components/CTASection';
+import CTASection from '../components/CTASection';
 import Footer from '@/components/Footer';
 
 const Index = () => {
